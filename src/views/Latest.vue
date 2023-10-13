4cd7db39193a621e0c31d84fa47f0e58336d0269
semajame <jamesanquillano@gmail.com>
2023-10-13 11:08:36 +0800

    update

--- src/views/Latest.vue
+++ src/views/Latest.vue
@@ -2,18 +2,18 @@
   <Header />
   <h1 class="trending">Latest</h1>
   <section class="latest__container">
-    <MoreBlogsVue />
+    <MoreBlogs />
   </section>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
-import MoreBlogsVue from "@/components/MoreBlogs.vue";
+import MoreBlogs from "../components/MoreBlogs.vue";
 export default {
   name: "Latest",
   components: {
     Header,
-    MoreBlogsVue,
+    MoreBlogs,
   },
 };
 </script>
